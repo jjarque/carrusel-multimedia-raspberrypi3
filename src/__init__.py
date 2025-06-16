0494e4e336d6fef0b95bf223ab9a6b2f6ba40661
Jaime <jaimejarquegutierrez1@gmail.com>
2025-06-16 20:56:47 +0200

    Create file __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+# src/__init__.py
+"""
+Paquete principal del Carrusel Multimedia
+"""
+
+from .carrusel_app import CarruselApp
+from .media_player import MediaPlayer
+from .usb_detector import USBDetector
+
+__version__ = "1.0.0"
+__author__ = "jjarque"
